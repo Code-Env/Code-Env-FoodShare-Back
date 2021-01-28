097ae9ce972e7b6cd6362a569f76a83cec2600b5
kimtaegwan <ab154@naver.com>
2021-01-29 02:01:43 +0900

    Add chat server

--- src/server.js
+++ src/server.js
@@ -22,8 +22,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", api);
 
-io.on("connection", () => {
-  /* ... */
+io.on("connection", socket => {
+  console.log("연결된 socketID : ", socket.id);
+  io.to(socket.id).emit("my socket id", { socketId: socket.id });
+
+  socket.on("enter chatroom", () => {
+    console.log("누군가가 입장함");
+    socket.broadcast.emit("receive chat", {
+      type: "alert",
+      chat: "누군가가 입장하였습니다.",
+      regData: Date.now(),
+    });
+  });
+  socket.on("send chat", data => {
+    console.log(`${socket.id}: ${data.chat}`);
+    io.emit("receive chat", data);
+  });
+  socket.on("leave chatroom", data => {
+    console.log("leave chatroom", data);
+    socket.broadcast.emot("receive chat", {
+      type: "alert",
+      chat: "누군가가 퇴장하였습니다",
+      regData: Date.now(),
+    });
+  });
 });
 
 app.listen(port, () => {
