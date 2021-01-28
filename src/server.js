704dfb11271f19aef035e1949a4388d2e1b17b73
kimtaegwan <ab154@naver.com>
2021-01-29 00:32:55 +0900

    Add socket.io

--- src/server.js
+++ src/server.js
@@ -10,6 +10,10 @@ const api = require("./api");
 const app = express();
 const cors = require("cors");
 
+// 채팅
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
 const port = process.env.PORT || 8080;
 
 app.use(cors({ origin: true })); // 자기와 다른 origin 접근 가능하도록 허용
@@ -18,7 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", api);
 
+io.on("connection", () => {
+  /* ... */
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
-    db.connect();
-  });
+  console.log(`Example app listening on port ${port}!`);
+  db.connect();
+});
